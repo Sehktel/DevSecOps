5a5e8a5ec8e96b03473492e3823a1a7de65676d5
Sehktel <suhov.aa@hotmail.com>
2023-04-09 15:24:40 +0300

    oops!

--- Math.cpp
+++ Math.cpp
@@ -1,17 +1,6 @@
 #include "Math.h"
 
-double Math::add(double num1, double num2) {
-    return num1 - num2;
-}
-
-double Math::subtract(double num1, double num2) {
-    return num1 - num2;
-}
-
-double Math::multiply(double num1, double num2) {
-    return num1 * num2;
-}
-
-double Math::divide(double num1, double num2) {
-    return num1 / num2;
-}
+double Math::add(double num1, double num2) { return num1 + num2; }
+double Math::subtract(double num1, double num2) { return num1 + num2; }
+double Math::multiply(double num1, double num2) { return num1 * num2; }
+double Math::divide(double num1, double num2) { return num1 / num2; }
